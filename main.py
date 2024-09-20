879a5476dccf747e678b56d863d546b9dc110206
Rashid Al-Qobbaj <1202474@student.birzeit.edu>
2024-09-20 17:02:30 +0300

    Arabic and English recognition ✅

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Start")
+import speech_recognition
+import pyttsx3
+
+recognizer = speech_recognition.Recognizer()
+
+while True:
+    try:
+        with speech_recognition.Microphone() as mic:
+            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+            audio = recognizer.listen(mic)
+            message = recognizer.recognize_google(audio, language='ar-EG')
+            print(message)
+    except speech_recognition.RequestError:
+        print("Network error")
+    except speech_recognition.UnknownValueError:
+        print("Unable to recognize speech")
+    except speech_recognition.WaitTimeoutError:
+        print("Timeout error")
+    except KeyboardInterrupt:
+        break
+
